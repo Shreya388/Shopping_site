a6a2082d558451f7e7d5250e79dfa8afdb6e5d14
Shreya388 <meenaxjha@gmail.com>
2021-10-09 23:59:58 +0530

    trying to work with props

--- src/index.js
+++ src/index.js
@@ -3,43 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Parent component
-class Parent extends React.Component{
-  render(){
-    return(
-      <div>
-        <h2>You are inside parent component</h2>
-        <Child name="cutiepie" userId = "3444" age="10" height="10 inch" />
-      </div>
-    );
-  }
+function Welcome(props) {
+  return <button>Hola, {props.name}!! You are {props.age}</button>;
 }
 
-//Child component
-class Child extends React.Component{
-  render(){
-    return(
-      <div>
-        <h2>Hello, {this.props.name}</h2>
-        <h2>He is {this.props.age} years old</h2>
-        <h3>You are inside Child component</h3>
-        <h3>His height is {this.props.height}</h3>
-        <h3>Your user id is: {this.props.userId}</h3>
-      </div>
-    );
-  }
-}
-
+const element = <Welcome name="Bucchi jha" age ="20" />;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Parent />
-  </React.StrictMode>,
+    
+  element,
   document.getElementById('root')
-  ,
-
-  console.log("Hello!")
 );
 
 // If you want to start measuring performance in your app, pass a function
